42a877105181b524494b8aa9b67acd1b6c1777e8
Willson Fernandes <willsonfeds@gmail.com>
2016-10-23 21:39:50 +0530

    modified file

---
+++ python1.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+print ("hello world")
+print ("thsi is my first python program")
+
+now = datetime.now()
+
+print ("todays date is %s-%s-%s " % (now.day, now.month, now.year))
